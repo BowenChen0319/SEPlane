eccde2a5d938b144854570e08af3ee9594e796de
Dennis Eichner <dennis.eichner@stud.uni-due.de>
2019-10-30 21:54:18 +0100

    FluggesellschaftAnlegenController hinzugefuegt

---
+++ Sourcecode/seplane/src/main/java/de/paluno/Fluggesellschaftsmanager/FluggesellschaftAnlegenController.java
@@ -0,0 +1,34 @@
+package main.java.de.paluno.Fluggesellschaftsmanager;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class FluggesellschaftAnlegenController {
+
+    @FXML
+    private Label name_label;
+
+    @FXML
+    private Label land_label;
+
+    @FXML
+    private Label budget_label;
+
+    @FXML
+    private TextField name_textfield;
+
+    @FXML
+    private TextField land_textfield;
+
+    @FXML
+    private TextField budget_textfield;
+
+    @FXML
+    private Button anlegen_button;
+
+    @FXML
+    private Button abbrechen_button;
+
+}
