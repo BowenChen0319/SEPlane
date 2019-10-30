cf9d0ae4efa4afa7e98109eac47c4d6644404591
Dennis Eichner <dennis.eichner@stud.uni-due.de>
2019-10-30 22:06:08 +0100

    FlugzeugkaufenController hinzugefuegt

---
+++ Sourcecode/seplane/src/main/java/de/paluno/Fluggesellschaftsmanager/FlugzeugKaufenController.java
@@ -0,0 +1,35 @@
+package main.java.de.paluno.Fluggesellschaftsmanager;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.ScrollPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+
+public class FlugzeugKaufenController; {
+
+@FXML
+private ScrollPane scrillpane;
+
+@FXML
+private TableView<?> tableview;
+
+@FXML
+private TableColumn<?, ?> modell_column;
+
+@FXML
+private TableColumn<?, ?> reichweite_column;
+
+@FXML
+private TableColumn<?, ?> sitzplaetze_column;
+
+@FXML
+private TableColumn<?, ?> preis_column;
+
+@FXML
+private Button kaufen_button;
+
+@FXML
+private Button abbrechen_button;
+
+        }
