a76b6c89e3afa0156f0f0611b85940657192206e
Dennis Eichner <dennis.eichner@stud.uni-due.de>
2019-11-05 16:19:10 +0100

    Projekt neu aufsetzen

--- Sourcecode/seplane/out/artifacts/SE_Plane_jar/lauch.sh
+++
@@ -1,1 +0,0 @@
-java --module-path /Library/Java/Extensions/javafx-sdk-13.0.1/lib --add-modules javafx.controls,javafx.fxml -jar SE-Plane.jar
--- Sourcecode/seplane/src/main/java/Models/Intervall.java
+++
@@ -1,5 +0,0 @@
-package Models;
-
-public enum Intervall {
-	TAG, WOCHE, MONAT;
-}
