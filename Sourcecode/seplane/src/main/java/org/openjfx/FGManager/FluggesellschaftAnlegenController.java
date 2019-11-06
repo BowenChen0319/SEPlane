68894ce0a57175751d7fca5967c088a09dcb471a
Dennis Eichner <dennis.eichner@stud.uni-due.de>
2019-11-06 12:52:53 +0100

    handleFluggesellschaft anlegen Logik erweitert, DB Befehle fehlen noch handleAbbrechen implementiert

--- Sourcecode/seplane/src/main/java/org/openjfx/FGManager/FluggesellschaftAnlegenController.java
+++ Sourcecode/seplane/src/main/java/org/openjfx/FGManager/FluggesellschaftAnlegenController.java
@@ -1,9 +1,9 @@
 package org.openjfx.FGManager;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
+import javafx.stage.Stage;
 
 public class FluggesellschaftAnlegenController {
 
@@ -31,5 +31,38 @@ public class FluggesellschaftAnlegenController {
     @FXML
     private Button abbrechen_button;
 
+    public void  handleFluggesellschaftAnlegen(ActionEvent event){
+        if (name_textfield.getText()!=null && land_textfield.getText()!=null && budget_textfield!=null) {
+            String name = name_textfield.getText();
+            String land = land_textfield.getText();
+            String budget = budget_textfield.getText();
 
+            //Datenbankbefehle
+
+            //Stage stage = (Stage) anlegen_button.getScene().getWindow();
+            //stage.close();
+
+        }
+        else{
+            String errorMessage = "Bitte fuellen Sie folgende Felder aus: ";
+            if (name_textfield.getText()==null){
+                errorMessage = errorMessage + "Name ";
+            }
+            else if (land_textfield.getText()==null){
+                errorMessage = errorMessage + "Land ";
+            }
+            else{
+                errorMessage = errorMessage + "Budget ";
+            }
+
+
+            Alert alert = new Alert(Alert.AlertType.ERROR, errorMessage, ButtonType.CLOSE);
+            alert.showAndWait();
+        }
+    }
+
+    public void handleAbbrechen (ActionEvent event){
+        Stage stage = (Stage) abbrechen_button.getScene().getWindow();
+        stage.close();
+    }
 }
