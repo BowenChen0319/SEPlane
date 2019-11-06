8ebdabff9dd7638d0619dca6267434d807c133f5
Dennis Eichner <dennis.eichner@stud.uni-due.de>
2019-11-06 21:39:37 +0100

    Flugzeug kaufen Struktur der Logik angelegt; DB-Befehle fehlen noch

--- Sourcecode/seplane/src/main/java/org/openjfx/FGManager/FlugzeugKaufenController.java
+++ Sourcecode/seplane/src/main/java/org/openjfx/FGManager/FlugzeugKaufenController.java
@@ -2,16 +2,13 @@ package org.openjfx.FGManager;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.Button;
-import javafx.scene.control.ScrollPane;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
+import javafx.scene.control.*;
 import javafx.stage.Stage;
 
 public class FlugzeugKaufenController {
 
 @FXML
-private ScrollPane scrillpane;
+private ScrollPane scrollpane;
 
 @FXML
 private TableView<?> tableview;
@@ -37,7 +34,28 @@ private Button abbrechen_button;
 
 
         public void handleKaufen(ActionEvent event){
+                if (0==0){  //ueberpruefen ob eine Zeile aus der Tabelle ausgewaehlt wurde
+                       //ausgewaehltes Flugzeug laden
+                        //Preis des ausgewaehlten Flugzeugs laden
+                        //Budget der ausgewaehlten FG laden
 
+                        if (0==0){ //ueberpruefen ob Preis hoeher als Budget ist
+                                //Budget der FG um den Preis des Flugzeugs reduzieren
+                                //neues Flugzeug des entsprechenden Modells erzeugen
+                                //Flugzeug der FG zuordnen
+                        }
+                        else{
+                                String errorMessage = "Leider verfuegt Ihre Fluggesellschaft nicht ueber ein ausreichendes Budget, um das ausgewaehlte Flugzeug zu kaufen.";
+                                Alert alert = new Alert(Alert.AlertType.ERROR, errorMessage, ButtonType.CLOSE);
+                                alert.showAndWait();
+                        }
+
+                }
+                else{
+                        String errorMessage = "Bitte waehlen Sie das Flugzeug, das Sie kaufen wollen, in der Tabelle aus.";
+                        Alert alert = new Alert(Alert.AlertType.ERROR, errorMessage, ButtonType.CLOSE);
+                        alert.showAndWait();
+                }
         }
 
         public void handleAbbrechen (ActionEvent event){
