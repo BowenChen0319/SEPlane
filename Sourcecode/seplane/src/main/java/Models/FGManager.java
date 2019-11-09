5eda58b6ac544f4336934f8762582808f344fc43
Guelistan Utkun <guelistan.utkun@stud.uni-due.de>
2019-11-09 13:57:06 +0100

    FGManager Model bearbeitet.

---
+++ Sourcecode/seplane/src/main/java/Models/FGManager.java
@@ -0,0 +1,37 @@
+package Models;
+
+public class FGManager {
+    
+    private Benutzer benutzername;
+    private String passwort;
+    private String land;
+    private Double budget;
+    private Fluggesellschaft fluggesellschaft;
+    
+
+    public FGManager() {
+    }
+    public FGManager(Benutzer benutzername, String passwort, Fluggesellschaft fluggesellschaft) {
+        
+        this.benutzername = benutzername;
+        this.passwort = passwort;
+        this.fluggesellschaft = fluggesellschaft;
+      
+    }
+    
+    public Benutzer getBenutzername() {
+        return benutzername;
+    }
+
+    public String getPasswort() {
+        return passwort;
+    }
+
+    public void setPasswort(String passwort) {
+        this.passwort = passwort;
+    }
+
+    public void setFluggesellschaft(Fluggesellschaft fluggesellschaft) {
+        this.fluggesellschaft = fluggesellschaft;
+    }
+}
