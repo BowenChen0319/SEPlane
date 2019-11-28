e7d19bae7976421d2dbdbde85907ced2fa03a32f
Dennis Eichner <dennis.eichner@stud.uni-due.de>
2019-11-28 17:24:39 +0100

    Update Flug anlegen

--- Sourcecode/seplane/src/main/java/Controller/FGM_FluegeInstanziierenController.java
+++ Sourcecode/seplane/src/main/java/Controller/FGM_FluegeInstanziierenController.java
@@ -189,8 +189,8 @@ public class FGM_FluegeInstanziierenController implements Initializable {
     public void flugAnlegen(Date date){
         Flug flug = new Flug();
         flug.setFluglinie(flugLinie);
-        //flug.setRestBusiness(flugLinie.getAnzb());
-        //flug.setRestEconomy(flugLinie.getAnze());
+        flug.createReserviereBusiness(flugLinie.getAnzb());
+        flug.createReserviereEconomy(flugLinie.getAnze(), flugLinie.getAnzb());
         date.setHours(stunde_choiceBox.getValue());
         date.setMinutes(minute_choiceBox.getValue());
         flug.setStartzeit(date);
