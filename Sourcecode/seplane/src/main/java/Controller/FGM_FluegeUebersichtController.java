946ab0f263a719b3fac526bbf2a02180db46d209
Dennis Eichner <dennis.eichner@stud.uni-due.de>
2019-12-03 17:54:03 +0100

    Flug Rentabilitaet implementiert - muss noch weiter getestet werden

--- Sourcecode/seplane/src/main/java/Controller/FGM_FluegeUebersichtController.java
+++ Sourcecode/seplane/src/main/java/Controller/FGM_FluegeUebersichtController.java
@@ -3,6 +3,7 @@ package Controller;
 import Models.Flug;
 import Models.Fluglinie;
 import javafx.beans.property.SimpleDoubleProperty;
+import javafx.beans.property.SimpleIntegerProperty;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -14,11 +15,13 @@ import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.stage.Stage;
+import jdk.jfr.Frequency;
 import org.openjfx.App;
 import org.openjfx.DBManager;
 
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.ResourceBundle;
 
 public class FGM_FluegeUebersichtController implements Initializable {
@@ -36,9 +39,6 @@ public class FGM_FluegeUebersichtController implements Initializable {
     private TableColumn<Flug, String> intervall_column;
 
     @FXML
-    private TableColumn<Flug, String> jungfernflug_column;
-
-    @FXML
     private TableColumn<Flug, String> Flugzeug_column;
 
     @FXML
@@ -48,13 +48,18 @@ public class FGM_FluegeUebersichtController implements Initializable {
     private TableColumn<Flug, Integer> businessPlaetze_column;
 
     @FXML
-    private TableColumn<Flug, String> preisEconomy_column;
+    private TableColumn<Flug, Date> abflugdatum_column;
 
     @FXML
-    private TableColumn<Flug, String> preisBusiness_column;
+    private TableColumn<Flug, String> rentabilitaet_column;
+
 
     @FXML
     private Button abbrechen_button;
+
+    @FXML
+    private Button flugStornieren_button;
+
 
     private Fluglinie fluglinie;
 
@@ -106,17 +111,8 @@ public class FGM_FluegeUebersichtController implements Initializable {
                 return simpleStringProperty;
             }
         });
-        jungfernflug_column.setCellValueFactory(cellData->{
-            if(cellData.getValue()==null){
-                return new SimpleStringProperty("");
-            }
-            else{
-                SimpleStringProperty simpleStringProperty = new SimpleStringProperty();
-                simpleStringProperty.setValue(cellData.getValue().getFluglinie().getStartdatum().toString());
-                return simpleStringProperty;
-            }
-        });
-        Flugzeug_column.setCellValueFactory(new PropertyValueFactory<>(""));
+
+
         Flugzeug_column.setCellValueFactory(cellData->{
             if(cellData.getValue()==null){
                 return new SimpleStringProperty("");
@@ -131,26 +127,16 @@ public class FGM_FluegeUebersichtController implements Initializable {
 
         economyPlaetze_column.setCellValueFactory(new PropertyValueFactory<>("restEconomy"));
         businessPlaetze_column.setCellValueFactory(new PropertyValueFactory<>("restBusiness"));
+        abflugdatum_column.setCellValueFactory(new PropertyValueFactory<>("startzeit"));
 
-
-        preisEconomy_column.setCellValueFactory(cellData->{
+        rentabilitaet_column.setCellValueFactory(cellData->{
             if(cellData.getValue()==null){
                 return new SimpleStringProperty("");
             }
             else{
-                SimpleStringProperty simpleStringProperty = new SimpleStringProperty();
-                simpleStringProperty.setValue(cellData.getValue().getFluglinie().getPreisee().toString());
-                return simpleStringProperty;
-            }
-        });
-        preisBusiness_column.setCellValueFactory(cellData->{
-            if(cellData.getValue()==null){
-                return new SimpleStringProperty("");
-            }
-            else{
-                SimpleStringProperty simpleStringProperty = new SimpleStringProperty();
-                simpleStringProperty.setValue(cellData.getValue().getFluglinie().getPreiseb().toString());
-                return simpleStringProperty;
+                SimpleStringProperty simpleDoubleProperty= new SimpleStringProperty();
+                simpleDoubleProperty.setValue(this.berechneRentabilitaetalsString(cellData.getValue()));
+                return simpleDoubleProperty;
             }
         });
 
@@ -165,4 +151,41 @@ public class FGM_FluegeUebersichtController implements Initializable {
         stage.close();
     }
 
+
+    public double berechneRentabilitaet(Flug flug){
+
+        Double kerosinkosten = 3.58 * (flug.getFluglinie().getEntfernung() / 100) * flug.getFluglinie().getFlugzeug().getSeats();
+
+        Integer gebuchtePlaetzeEconomy = flug.getFluglinie().getAnze() - flug.getRestEconomy();
+        Integer gebuchtePlaetzeBusiness = flug.getFluglinie().getAnzb() - flug.getRestBusiness();
+        Double einnahmen = (gebuchtePlaetzeEconomy * flug.getFluglinie().getPreisee()) + (gebuchtePlaetzeBusiness * flug.getFluglinie().getPreiseb());
+
+        Double flughafenpauschale = 0.05 * (flug.getFluglinie().getStart().getRunway_length() + flug.getFluglinie().getZiel().getRunway_length());
+
+        Double gewinn = kerosinkosten + einnahmen + flughafenpauschale;
+
+        return gewinn;
+    }
+
+    public String berechneRentabilitaetalsString(Flug flug){
+
+        Double kerosinkosten = 3.58 * (flug.getFluglinie().getEntfernung() / 100) * flug.getFluglinie().getFlugzeug().getSeats();
+
+        Integer gebuchtePlaetzeEconomy = flug.getFluglinie().getAnze() - flug.getRestEconomy();
+        Integer gebuchtePlaetzeBusiness = flug.getFluglinie().getAnzb() - flug.getRestBusiness();
+        Double einnahmen = (gebuchtePlaetzeEconomy * flug.getFluglinie().getPreisee()) + (gebuchtePlaetzeBusiness * flug.getFluglinie().getPreiseb());
+
+        Double flughafenpauschale = 0.05 * (flug.getFluglinie().getStart().getRunway_length() + flug.getFluglinie().getZiel().getRunway_length());
+
+        Double gewinn =  einnahmen - flughafenpauschale - kerosinkosten;
+
+        String gewinnAlsString = gewinn.toString();
+
+        return gewinnAlsString;
+    }
+
+
+    public void handleFlugStornieren (ActionEvent event){
+
+    }
 }
