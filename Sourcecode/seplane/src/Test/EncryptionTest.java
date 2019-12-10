952a67866170eaa383f1874987ee33b765c7f182
Chen Bowen <chen.bowen@uni-due.de>
2019-12-10 13:04:26 +0100

    上传新文件

---
+++ Sourcecode/seplane/src/Test/EncryptionTest.java
@@ -0,0 +1,21 @@
+package Toolbox;
+
+import org.apache.commons.lang3.RandomStringUtils;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class EncryptionTest {
+
+    @Test
+    void check() throws Exception {
+
+        for (int i = 0; i < 10; i++) {
+            String str = RandomStringUtils.randomAlphanumeric(8);
+            String stored = "";
+            stored = Encryption.getSaltedHash(str);
+            assertEquals(true, new Encryption().check(str, stored));
+        }
+
+    }
+}
